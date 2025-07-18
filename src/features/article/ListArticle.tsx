07aa1d8664006d1abb4cd840983b70cae9e34fad
Paste Prosmana <pasteprosmana189@gmail.com>
2025-07-18 21:36:48 +0700

    fix: Search input keyword wrong

--- src/features/article/ListArticle.tsx
+++ src/features/article/ListArticle.tsx
@@ -10,7 +10,7 @@ const ListArticle = ({ article }: { article: ArticleDTO }) => {
       className="shadow-md hover:bg-teal-50 border-b-4 min-h-[260px] border-b-black/20 border-r-4 border-r-black/20 backdrop-blur-sm rounded-2xl bg-white/30 flex flex-col"
     >
       <div className="px-4 py-2 flex-col">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-md md:text-2xl font-bold">
           {sliceString(article.headline.main, 100)}
         </h1>
         <p className="mt-2 text-md">
@@ -18,7 +18,7 @@ const ListArticle = ({ article }: { article: ArticleDTO }) => {
             ? "By Anonymous"
             : sliceString(article.byline.original, 40)}
         </p>
-        <small className="block mt-1 text-md">
+        <small className="block mt-1 text-xs md:text-md">
           {formatDate(article.pub_date)}
         </small>
         <p className="my-2">{sliceString(article.abstract, 160)} ...</p>
